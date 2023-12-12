c6b812b636fd7c532feafd16a715f4c92061168a
k.solovev <k.solovev.work@gmail.com>
2023-12-12 21:26:49 +0300

    add burger-ingredients component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import AppHeader from './components/app-header/app-header';
+import BurgerIngredients from './components/burger-ingredients/burger-ingredients';
 
 function App() {
   return (
-    <AppHeader />
+    <>
+      <AppHeader />
+      <main>
+        <BurgerIngredients />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -0,0 +1,70 @@
+import { Tab, Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import styles from './burger-ingredients.module.css';
+import { data } from '../../utils/data'
+
+const BurgerIngredients = () => {
+  const buns = data.filter(el => el.type === 'bun')
+  const sauces = data.filter(el => el.type === 'sauce')
+
+  return (
+    <section className={styles.ingredients}>
+      <h2 className='text text_type_main-large mb-5'>Соберите бургер</h2>
+
+      <div className={`${styles.ingredients__tabs} mb-10`}>
+        <Tab value="buns">Булки</Tab>
+        <Tab value="sauces">Соусы</Tab>
+        <Tab value="fillings">Начинки</Tab>
+      </div>
+
+      <div className='ingredients__wrap'>
+        <div className='mb-10'>
+          <h3 className='text text_type_main-medium mb-6'>Булки</h3>
+          <ul className={`${styles.ingredients__list} pt-0 pr-4 pb-0 pl-4`}>
+            {buns.map(bun => {
+              return (
+                <li className={styles.ingredients__item}>
+                  <div className={`${styles.ingredients__item__top} mb-1`}>
+                    <img src={bun.image} />
+                    <Counter count={1} size="default" extraClass="m-1" />
+                  </div>
+                  <div className='ingredients__item__bottom'>
+                    <div className={`${styles.ingredients__item__price} mb-1`}>
+                      <span className="text text_type_digits-default mr-2">20</span>
+                      <CurrencyIcon type="primary" />
+                    </div>
+                    <p className={`${styles.ingredients__item__name} text text_type_main-default`}>{bun.name}</p>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div className='mb-10'>
+          <h3 className='text text_type_main-medium mb-6'>Соусы</h3>
+          <ul className={`${styles.ingredients__list} pt-0 pr-4 pb-0 pl-4`}>
+            {sauces.map(sauce => {
+              return (
+                <li className={styles.ingredients__item}>
+                  <div className={`${styles.ingredients__item__top} mb-1`}>
+                    <img src={sauce.image} />
+                    <Counter count={1} size="default" extraClass="m-1" />
+                  </div>
+                  <div className='ingredients__item__bottom'>
+                    <div className={`${styles.ingredients__item__price} mb-1`}>
+                      <span className="text text_type_digits-default mr-2">20</span>
+                      <CurrencyIcon type="primary" />
+                    </div>
+                    <p className={`${styles.ingredients__item__name} text text_type_main-default`}>{sauce.name}</p>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default BurgerIngredients
